7cff3d3024be2d47307038af6a86410fab34a819
gskundi87 <74677259+gskundi87@users.noreply.github.com>
2023-01-11 07:08:16 -0800

    revised fizzbuzz

--- Fizzbuzz/fizzbuzz.js
+++ Fizzbuzz/fizzbuzz.js
@@ -1,24 +1,3 @@
-let num = 0;
-function increment() {
-    const countElem = document.getElementById('count');
-    countElem.innerText = ++num;
-}
-function decrement() {
-    const countElem = document.getElementById('count');
-    countElem.innerText = --num;
-}
-function reset() {
-    const countElem = document.getElementById('count');
-    num = 0;
-    countElem.innerText = 0;
-}
-
-function modifyNum(number) {
-    const countElem = document.getElementById('count');
-    num += number;
-    countElem.innerText = num;
-}
-
 function fizzbuzz() {
     input = document.querySelector('#fizzbuzz-input').value
     const outputElem = document.getElementById('fizzbuzz-output');
@@ -53,11 +32,4 @@ function fizzbuzz() {
 
         outputElem.innerText = str;
     }
-    //you'll want to look up
-    //javascript loops (either while, or for will work well here)
-    //conditionals (if, elseif, else) or switch statements
-    //modulo operator will also come handy here.
-}
-
-function calculator(value) {
 }
