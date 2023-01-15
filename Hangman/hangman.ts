79ac7f7197a0bebaa5b783c7efbecfb12aeba470
gskundi87 <gskundi87@gmail.com>
2023-01-14 22:09:33 -0800

    added hangman

---
+++ Hangman/hangman.ts
@@ -0,0 +1,12 @@
+class Hangman {
+    private word: string;
+    public constructor() {
+        this.word = "";
+    }
+
+    public guess(letter: string) {
+
+    }
+}
+
+const hangman = new Hangman();
