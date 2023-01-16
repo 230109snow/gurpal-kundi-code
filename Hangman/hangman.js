561733d86e7a653013c679a789c94cd820ee5a4d
gskundi87 <gskundi87@gmail.com>
2023-01-16 15:08:45 -0800

    hangman update

--- Hangman/hangman.js
+++ Hangman/hangman.js
@@ -1,13 +1,27 @@
 var Hangman = /** @class */ (function () {
     function Hangman(word) {
         this.word = word;
+        this.guessStr = "";
+        var guessDiv = document.getElementById('guessDiv');
+        for (var i = 0; i < this.word.length; i++) {
+            this.guessStr += "_";
+        }
+        this.printGuessStr(guessDiv);
     }
-    Hangman.prototype.guess = function (letter) {
+    Hangman.prototype.checkGuess = function (letter) {
     };
-    Hangman.prototype.printOut = function () {
-        console.log(this.word);
+    Hangman.prototype.printGuessStr = function (divElement) {
+        var tempStr = "";
+        console.log(this.guessStr);
+        console.log(this.guessStr.length);
+        for (var i = 0; i < this.guessStr.length; i++) {
+            tempStr += this.guessStr[i] + " ";
+        }
+        console.log(tempStr);
+        if (divElement != null) {
+            divElement.innerText = tempStr;
+        }
     };
     return Hangman;
 }());
 var hangman = new Hangman("apple");
-hangman.printOut();
--- Hangman/hangman.ts
+++ Hangman/hangman.ts
@@ -1,18 +1,48 @@
 class Hangman {
     private word: string;
+    private guessStr: string;
+    private guesses: number;
+
     public constructor(word: string) {
         this.word = word;
+        this.guessStr = "";
+        this.guesses = 0;
+
+        const guessDiv = document.getElementById('guessDiv');
+
+        for (let i: number = 0; i < this.word.length; i++)
+        {
+            this.guessStr += "_";
+        }
+
+        this.printGuessStr(guessDiv);
     }
 
-    public guess(letter: string) {
+    public checkGuess(letter: string) {
 
     }
 
-    public printOut() {
-        console.log(this.word);
+    public checkWinLoss() {
+        
+    }
+
+    public printGuessStr(divElement: HTMLElement | null) {
+        let tempStr: string = "";
+        console.log(this.guessStr);
+        console.log(this.guessStr.length);
+
+        for (let i: number = 0; i < this.guessStr.length; i++)
+        {
+            tempStr += this.guessStr[i] + " ";
+        }
+
+        console.log(tempStr);
+
+        if (divElement != null) {
+            divElement.innerText = tempStr;
+        }
     }
 }
 
 const hangman = new Hangman("apple");
-hangman.printOut();
 
