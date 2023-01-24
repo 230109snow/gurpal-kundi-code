e56a482da3095946aaacd8f3b8cec763d33b56b2
gskundi87 <74677259+gskundi87@users.noreply.github.com>
2023-01-24 14:41:55 -0800

    new

---
+++ Gurpal-Kundi-PS0/ng-nasa-app/src/api-key.ts
@@ -0,0 +1,4 @@
+// live_uGiU7aVVrvVszmrFLuoQehGhapzJtyigNrBLzdgm6FuxtH3qKcRgRPx3yJ2ZuiH9
+export const apikey = "live_uGiU7aVVrvVszmrFLuoQehGhapzJtyigNrBLzdgm6FuxtH3qKcRgRPx3yJ2ZuiH9";
+export const NASAAPI = "Hy65zapkRNx28twZR7tPgA0e8S6EYt8c90NcVXU1";
+export const apiroot = "https://api.thecatapi.com/v1";
