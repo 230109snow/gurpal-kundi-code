b26c18eaf0980e7bd9f42d1348548da8f0e66bd2
gskundi87 <gskundi87@gmail.com>
2023-01-24 23:19:08 -0800

    PS0 search

--- Gurpal-Kundi-PS0/ng-nasa-app/src/app/app.module.ts
+++ Gurpal-Kundi-PS0/ng-nasa-app/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TopicSearchComponent } from './topic-search/topic-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    TopicSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Gurpal-Kundi-PS0/ng-nasa-app/src/app/topic-search/topic-search.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { apikey } from 'src/api-key';
+
+@Component({
+  selector: 'app-topic-search',
+  templateUrl: './topic-search.component.html',
+  styleUrls: ['./topic-search.component.css']
+})
+export class TopicSearchComponent {
+
+  constructor(private http: HttpClient) {}
+
+  numPics : number = 0;
+  nasaPics : any[] = [];
+  keyword : string = "";
+  now : number | Date = Date.now();
+
+  validation = {
+    required: true,
+    min : 1,
+    max : 25
+  }
+
+  getPics() : void {
+    const url = `https://images-api.nasa.gov/search?q=${this.keyword}&media_type=image&page_size=1000`;
+
+    this.http.get(url).subscribe((data: any) => {
+
+      console.log(data.collection.items.length);
+      this.nasaPics = data.collection.items.sort(() => 0.5 - Math.random()).slice(0, this.numPics); 
+      console.log(this.nasaPics);
+    })
+  }
+
+}
